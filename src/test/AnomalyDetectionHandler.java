7bc928dbc9c384ebb1ab8910d81db7f58192866a
amitha511 <amitha51111@gmail.com>
2022-01-07 16:07:17 +0200

    stone 3 - task 2 done.

--- src/test/AnomalyDetectionHandler.java
+++ src/test/AnomalyDetectionHandler.java
@@ -4,11 +4,71 @@ package test;
 import test.Commands.DefaultIO;
 import test.Server.ClientHandler;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class AnomalyDetectionHandler implements ClientHandler{
 
-	public class SocketIO implements DefaultIO{
 
+	@Override
+	public void handleClient(InputStream inFromClient, OutputStream outToClient) {
+		SocketIO socketIO = new SocketIO(inFromClient,outToClient);
+		CLI obj = new CLI(socketIO);
+		obj.start();
+		obj.dio.write("bye\n");
+		socketIO.close();
 	}
 
+	public class SocketIO implements DefaultIO {
+
+		BufferedReader in;
+		PrintWriter out;
+		public SocketIO(InputStream inFromClient,OutputStream outToClient) {
+				in = new BufferedReader(new InputStreamReader(inFromClient));
+				out = new PrintWriter(outToClient, true);
+
+		}
+
+		@Override
+		public String readText() {
+			try {
+				return in.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		public void write(String text) {
+			out.print(text);
+			out.flush();
+		}
+
+		@Override
+		public float readVal() {
+			try {
+				return (float)in.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+		@Override
+		public void write(float val) {
+			out.print(val);
+		}
+
+		public void close() {
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			out.close();
+		}
+	}
 
 }
--- src/test/Server.java
+++ src/test/Server.java
@@ -1,9 +1,20 @@
 package test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+import test.AnomalyDetectionHandler.SocketIO;
+
 public class Server {
 
 	public interface ClientHandler{
-		// define...
+		void handleClient(InputStream inFromClient, OutputStream outToClient);
+
 	}
 
 	volatile boolean stop;
@@ -13,7 +24,23 @@ public class Server {
 	
 	
 	private void startServer(int port, ClientHandler ch) {
-		// implement here the server...
+		//ch =new AnomalyDetectionHandler();
+		try {
+			ServerSocket server = new ServerSocket(port);
+			server.setSoTimeout(1000);
+			while(!stop) {
+				try {
+					Socket aClient = server.accept();
+					ch.handleClient(aClient.getInputStream(), aClient.getOutputStream());
+					aClient.close();
+				} catch (SocketTimeoutException e) {}
+			}
+			server.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			// implement here the server...
+
 	}
 	
 	// runs the server in its own thread
